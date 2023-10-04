5754d5e648bfe482f5f73af4da8f667d5a2936f3
Marat-Tim <rurikcat@gmail.com>
2023-10-04 20:20:20 +0300

    начальный коммит

---
+++ src/main/java/ru/marattim/onlineauctionspringboot/OnlineAuctionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.marattim.onlineauctionspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAuctionSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineAuctionSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/marattim/onlineauctionspringboot/OnlineAuctionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.marattim.onlineauctionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAuctionSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
